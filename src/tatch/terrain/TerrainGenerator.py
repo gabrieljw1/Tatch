e73f79f6f5be8f45d706d448e4f17f47015717a1
Gabriel Wong <gabrieljw1@gmail.com>
2019-11-25 14:33:55 -0500

    TerrainGen/PerlinGen Consolodation
    Merged both files into one

--- src/tatch/terrain/TerrainGenerator.py
+++ src/tatch/terrain/TerrainGenerator.py
@@ -1,12 +1,36 @@
-from terrain.PerlinGenerator import PerlinGenerator
 from matrix.Vector import Vector
+import numpy as np
+import noise
 
 class TerrainGenerator(object):
-    def __init__(self, dims, scale):
-        self.perlinGenerator = PerlinGenerator(dims, scale)
+    def __init__(self, dims, scale, pOctaves=6, pPersistence=0.5, pLacunarity=2.0):
+        self.dims = dims
+        self.scale = scale
+        self.pOctaves = pOctaves
+        self.pPersistence = pPersistence
+        self.pLacunarity = pLacunarity
+
+    def generateGrid(self, xShift, yShift):
+        grid = np.zeros(self.dims)
+        (width, height) = (self.dims)
+
+        for x in range(width):
+            scaledShiftedX = (x + xShift) / self.scale
+
+            for y in range(height):
+                grid[x][y] = float(5.0*noise.pnoise2(scaledShiftedX,
+                                            (y + yShift)/self.scale,
+                                            octaves=self.pOctaves,
+                                            persistence=self.pPersistence,
+                                            lacunarity=self.pLacunarity,
+                                            repeatx=1024,
+                                            repeaty=1024,
+                                            base=0))
+
+        return grid
 
     def generateTerrainVectors(self, xOffset, yOffset, zOffset, zSign = 1.0, xShift=0.0, zShift=0.0):
-        perlinGrid = self.perlinGenerator.generateGrid(xShift, zShift)
+        perlinGrid = self.generateGrid(xShift, zShift)
 
         yeet = len(perlinGrid[0])
 
